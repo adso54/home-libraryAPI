f6397624660a6676c1d4aa443cbd055cf0355b47
kamil.danielski@gmail.com <kamil.danielski@gmail.com>
2020-12-22 06:56:16 +0100

    Creating upload file route

--- dbUtils/book.js
+++ dbUtils/book.js
@@ -1,9 +1,10 @@
 const { db } = require('./config');
 
-
 const addBook = (params) =>{
     return new Promise((resolve, reject) =>{
         const { createDate = new Date(), title, description, image} = params;
+        console.log(image)
+
         db('book')
         .insert({
             create_date: createDate,
@@ -18,12 +19,30 @@ const addBook = (params) =>{
     })
 }
 
+// const upload = (params) => {
+//     return new Promise((resolve, reject) => {
+
+//     })
+// }
+
 const getBook =(params) => {
     return new Promise((resolve, reject) => {
         db.select('*')
             .from('book')
-            .where('id', '=', '6')
-        .then(book => resolve(book))
+            .where('id', '=', '14')
+        .then(book => {
+            console.log(book[0].image)
+
+            var binary = '';
+            var bytes = new Uint8Array(book[0].image);
+            var len = bytes.byteLength;
+            for (var i = 0; i < len; i++) {
+                binary += String.fromCharCode( bytes[ i ] );
+            }
+            book[0].image= binary;
+            console.log(book)
+            resolve(book)
+        })
         .catch(err=>reject(err))
     })
 }
--- routes/book.js
+++ routes/book.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 // const { dbType} = require('../dbUtils/type');
 const { dbBook}= require('../dbUtils/book');
+const multer = require('multer')
 
 router.post('/', (req, res) => {
    dbBook.addBook(req.body)
@@ -15,4 +16,31 @@ router.get('/', (req, res) => {
    .catch((err) =>res.json(err))
 })
 
+
+var storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+   cb(null, 'public/images')
+ },
+ filename: function (req, file, cb) {
+   cb(null, Date.now() + '-' +file.originalname )
+ }
+})
+var upload = multer({ storage: storage }).single('file')
+
+router.post('/upload',(req, res) => {
+   upload(req, res, (err) =>{
+          if (err instanceof multer.MulterError) {
+              return res.status(500).json(err)
+          } else if (err) {
+              return res.status(500).json(err)
+          }
+     return res.status(200).send(req.file)
+
+   })
+
+});
+
 module.exports = router;
+
+
+
